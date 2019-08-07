e79c2be6666f3c20067792733061fe115003a37f
chenxinmeng-123 <aa@qq.com>
2019-08-07 15:06:26 +0800

    Initial commit

---
+++ Login/Login.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+driver=webdriver.Chrome()
+driver.maximize_window()
+
+driver.implicitly_wait(10)
+
+driver.get('http://chat.shopee.com/#/dashboard')
+'''
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[3]/div/button').click()
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[3]/div/button').send_keys('123')
+'''
+
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[1]/div/div[1]/input').click()
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[1]/div/div[1]/input').send_keys(chenxinmeng)
+driver.implicitly_wait(1)
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[2]/div/div[1]/input').click()
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[2]/div/div[1]/input').send_keys('123456')
+driver.implicitly_wait(1)
+driver.find_element_by_xpath('//*[@id="app"]/div/form/div[3]/div/button').click()
